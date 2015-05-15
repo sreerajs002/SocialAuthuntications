e17ab2b0c44a2bb225a29f719da5d89af81e3456
sreerajs002 <sreeraj.siddantam@gmail.com>
2015-05-15 16:09:33 +0530

    Update omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,9 +6,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :linkedin, "linkedin_key", "linkedin_secret"
   provider :google_oauth2, "google_key" ,"google_secret"
 end
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   provider :facebook,  '852108834849109', '84232837451bd57d4c4c314ada150206',
-#            :scope => 'email,user_birthday,read_stream', :display => 'popup'
-# end
-
-#,"852108834849109", "84232837451bd57d4c4c314ada150206",
